0ba438c8e648ff94bcbc10f696245f6ced678221
takahashi yuuhei <gonbee1230@gmail.com>
2020-06-25 02:56:39 +0900

    success postgreSQL & docker

---
+++ slack/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from slack.views import SingUpAccount, Login, ChatModel, SendChatMessage, Logout, DeleteView, Good, LoginUser
+
+urlpatterns = [
+    path('SignUp/', SingUpAccount, name="signup"),
+    path('Login/', Login, name="login"),
+    path('Chat/', ChatModel, name="chat"),
+    path('ChatSend/', SendChatMessage, name="sendchatmessage"),
+    path('Logout/', Logout, name="logout"),
+    path('Chat/<int:pk>/Delete', DeleteView, name="delete"),
+    path('Chat/<int:pk>/Good', Good, name="good"),
+    path('Loginuser/', LoginUser, name="loginuser")
+]
