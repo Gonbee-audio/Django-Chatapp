7f70f7b488b2fe312077ba14a1753f614e91f656
takahashi yuuhei <gonbee1230@gmail.com>
2020-06-29 05:59:48 +0900

    change chatbar position chat.hrml for base.html

--- slack/urls.py
+++ slack/urls.py
@@ -4,7 +4,7 @@ from slack.views import SingUpAccount, Login, ChatModel, SendChatMessage, Logout
 urlpatterns = [
     path('SignUp/', SingUpAccount, name="signup"),
     path('Login/', Login, name="login"),
-    path('Chat/', ChatModel, name="chat"),
+    path('', ChatModel, name="chat"),
     path('ChatSend/', SendChatMessage, name="sendchatmessage"),
     path('Logout/', Logout, name="logout"),
     path('Chat/<int:pk>/Delete', DeleteView, name="delete"),
