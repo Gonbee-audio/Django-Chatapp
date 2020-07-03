96ef633f98840bff5fe0a372a7c4da52bd65b5b2
takahashi yuuhei <gonbee1230@gmail.com>
2020-07-03 10:53:35 +0900

    you can find exist user func

--- ChatAdmin/settings.py
+++ ChatAdmin/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'slack.middleware.UserMiddleware',
 ]
 
 ROOT_URLCONF = 'ChatAdmin.urls'
--- slack/models.py
+++ slack/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.utils.functional import cached_property
 
 # Create your models here.
 
