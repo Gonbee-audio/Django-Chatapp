58a1a29384b50326b821d53fe93c538f1081f598
takahashi yuuhei <gonbee1230@gmail.com>
2020-07-04 14:56:29 +0900

    deploy version

--- slack/middleware.py
+++ slack/middleware.py
@@ -5,13 +5,11 @@ class UserMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # リクエストへの前処理を記述
-        # reqeust.profileに、Profileモデルからブログ情報を取得する
+    
+
         request.u = User.objects.all()
 
-        # 固定
+        
         response = self.get_response(request)
-
-        # レスポンスへの後処理を記述
 
         return response
