33a1123c38102062af159d8f08a90a460b16ac07
takahashi yuuhei <gonbee1230@gmail.com>
2020-07-06 06:22:27 +0900

    add resposive web design beta1.0

--- 名称未設定フォルダ/slack/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ChatMessage, Comments, SecredMessage
-
-admin.site.register(ChatMessage)
-admin.site.register(Comments)
-admin.site.register(SecredMessage)
-# Register your models here.
